c02a1e2058b3d527aec48ddc7e44b63196b901eb
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-09-04 15:00:51 +0800

    Initial Commit

---
+++ src/app/core/constants/constants.ts
@@ -0,0 +1,19 @@
+// Use to input the values in your endpoint in order for it to be dybamic:
+// e.g.
+// https://.../CarRentalApp/api/GetCars
+
+export const constant = {
+  MODULE_NAME: {
+    CAR: '',
+  },
+  API_METHODS: {
+    // Example:
+    CAR_RENTAL: {
+      GET_ALL_CARS: 'GetCars',
+      ADD_CARS: 'AddCars',
+    },
+    BOOKING: {
+      //
+    },
+  },
+};
