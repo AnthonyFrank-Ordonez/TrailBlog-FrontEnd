76b976750163fc89ee4c982bb324cff4d65952fa
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-09-04 15:41:07 +0800

    Created Layout and Added Index/Home Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Layout } from './shared/components/layout/layout';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: Home,
+      },
+    ],
+  },
+];
