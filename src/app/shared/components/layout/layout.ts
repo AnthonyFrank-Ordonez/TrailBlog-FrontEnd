b82ae1ce9efe8c54427d5cb3357dcc2f76dbc7ec
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-09-09 16:54:59 +0800

    Create side navigation

--- src/app/shared/components/layout/layout.ts
+++ src/app/shared/components/layout/layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "../header/header";
+import { Header } from '../header/header';
+import { SideNavigation } from '../side-navigation/side-navigation';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, Header],
+  imports: [Header, SideNavigation],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
