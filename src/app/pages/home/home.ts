171a753ab8a24d1fd246fcfc79605c87badc136f
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-09-10 15:37:15 +0800

    Added Dropdown sort for list of blogs

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { ZardDropdownDirective } from '@shared/components/dropdown/dropdown-trigger.directive';
+import { ZardDropdownMenuContentComponent } from '@shared/components/dropdown/dropdown-menu-content.component';
+import { ZardDropdownMenuLabelComponent } from '@shared/components/dropdown/dropdown-label.component';
+import { ZardDropdownMenuItemComponent } from '@shared/components/dropdown/dropdown-item.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    ZardDropdownDirective,
+    ZardDropdownMenuContentComponent,
+    ZardDropdownMenuLabelComponent,
+    ZardDropdownMenuItemComponent,
+  ],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
 })
 export class Home {
-
+  onSort() {
+    console.log('Sort Clicked!');
+  }
 }
