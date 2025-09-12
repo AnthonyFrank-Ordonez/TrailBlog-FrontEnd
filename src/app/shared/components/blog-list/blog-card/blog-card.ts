ac642ddc39e4da05e804fc831490aeae0502106a
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-09-12 20:39:13 +0800

    Added dropdown for three dots menu in blog card and share button

--- src/app/shared/components/blog-list/blog-card/blog-card.ts
+++ src/app/shared/components/blog-list/blog-card/blog-card.ts
@@ -1,10 +1,18 @@
 import { DatePipe } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { Blog } from 'src/app/core/models/interface/blogs';
+import { ZardDropdownMenuContentComponent } from '@shared/components/dropdown/dropdown-menu-content.component';
+import { ZardDropdownMenuItemComponent } from '@shared/components/dropdown/dropdown-item.component';
+import { ZardDropdownDirective } from '@shared/components/dropdown/dropdown-trigger.directive';
 
 @Component({
   selector: 'app-blog-card',
-  imports: [DatePipe],
+  imports: [
+    DatePipe,
+    ZardDropdownMenuContentComponent,
+    ZardDropdownMenuItemComponent,
+    ZardDropdownDirective,
+  ],
   templateUrl: './blog-card.html',
   styleUrl: './blog-card.css',
 })
