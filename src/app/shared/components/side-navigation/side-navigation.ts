817aaee828065bf5964abddb901e9e275140b4b3
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-09-12 20:43:49 +0800

    Added active design for side navigation during desktop mode

--- src/app/shared/components/side-navigation/side-navigation.ts
+++ src/app/shared/components/side-navigation/side-navigation.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-side-navigation',
   imports: [],
   templateUrl: './side-navigation.html',
-  styleUrl: './side-navigation.css'
+  styleUrl: './side-navigation.css',
 })
 export class SideNavigation {
-
+  activeTab = signal<string>('home');
 }
