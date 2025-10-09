9a56c0f175edc7564cbce919163aa066c3e84381
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-10-09 16:14:39 +0800

    Create UI for select community in create post route

--- src/app/pages/create-post/create-post.ts
+++ src/app/pages/create-post/create-post.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, signal, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-create-post',
   imports: [],
   templateUrl: './create-post.html',
-  styleUrl: './create-post.css'
+  styleUrl: './create-post.css',
 })
 export class CreatePost {
+  @ViewChild('searchInput') searchInput!: ElementRef;
+  @ViewChild('formContainer') formContainer!: ElementRef;
+  @ViewChild('toggleButton') toggleButton!: ElementRef;
 
+  constructor(private elementRef: ElementRef) {}
+  isCommunitySelectionSelected = signal<boolean>(false);
+
+  showCommunitySelection(): void {
+    this.isCommunitySelectionSelected.set(true);
+    setTimeout(() => {
+      if (this.searchInput) {
+        this.searchInput.nativeElement.focus();
+      }
+    }, 0);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (!this.isCommunitySelectionSelected()) return;
+
+    const clickedInsideForm = this.formContainer?.nativeElement.contains(event.target);
+    const clickedButton = this.toggleButton?.nativeElement.contains(event.target);
+
+    if (!clickedInsideForm && !clickedButton) {
+      this.isCommunitySelectionSelected.set(false);
+    }
+  }
 }
