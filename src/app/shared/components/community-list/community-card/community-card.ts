caf9ed95b39fed68e6b6639acbe5a964498c2023
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-10-18 11:14:12 +0800

    Implement leave community in communities page

--- src/app/shared/components/community-list/community-card/community-card.ts
+++ src/app/shared/components/community-list/community-card/community-card.ts
@@ -1,6 +1,12 @@
-import { Component, input } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, DestroyRef, inject, input } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Communities } from '@core/models/interface/community';
+import { CommunityService } from '@core/services/community.service';
+import { MessageService } from '@core/services/message.service';
+import { ModalService } from '@core/services/modal.service';
 import { InitialsPipe } from '@shared/pipes/initials-pipe';
+import { handleHttpError } from '@shared/utils/utils';
 
 @Component({
   selector: 'app-community-card',
@@ -9,5 +15,32 @@ import { InitialsPipe } from '@shared/pipes/initials-pipe';
   styleUrl: './community-card.css',
 })
 export class CommunityCard {
+  private communityService = inject(CommunityService);
+  private messageService = inject(MessageService);
+  private modalService = inject(ModalService);
+  private destroyRef = inject(DestroyRef);
+
   community = input.required<Communities>();
+
+  onLeaveCommunity(): void {
+    this.modalService.openModal({
+      type: 'community',
+      title: 'Leave Community',
+      description: 'Are you sure you want to leave this community?',
+      content: 'leave-community',
+      data: { communityId: this.community().id },
+      onConfirm: (communityId) => this.leaveCommunity(communityId),
+    });
+  }
+
+  leaveCommunity(communityId: string): void {
+    this.communityService
+      .leaveCommunity(communityId)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        error: (error: HttpErrorResponse) => {
+          handleHttpError(error, this.messageService);
+        },
+      });
+  }
 }
