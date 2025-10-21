4e924c9759144403fd653f0ef4590eb4e442a41c
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-10-21 15:38:07 +0800

    Fix the bug on like and dislike button

--- src/app/shared/components/post-list/post-list.ts
+++ src/app/shared/components/post-list/post-list.ts
@@ -1,10 +1,11 @@
-import { Component, computed, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { PostCard } from './post-card/post-card';
 import { PostService } from '@core/services/post.service';
 import { ZardDividerComponent } from '@shared/components/divider/divider.component';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { handleHttpError } from '@shared/utils/utils';
 import { MessageService } from '@core/services/message.service';
+import { AuthService } from '@core/services/auth.service';
 
 @Component({
   selector: 'app-post-list',
@@ -15,12 +16,24 @@ import { MessageService } from '@core/services/message.service';
 export class PostList implements OnInit {
   private readonly postService = inject(PostService);
   private messageService = inject(MessageService);
+  private authService = inject(AuthService);
   private destroyRef = inject(DestroyRef);
 
+  isAuthenticated = this.authService.isAuthenticated;
   posts = this.postService.posts;
 
   isPostLoading = this.postService.isPostLoading;
   skeletonArray = Array(5).fill(0);
+
+  // constructor() {
+  //   effect(() => {
+  //     const isAuth = this.isAuthenticated();
+
+  //     if (!isAuth) {
+  //       this.loadPosts();
+  //     }
+  //   });
+  // }
 
   ngOnInit(): void {
     this.loadPosts();
