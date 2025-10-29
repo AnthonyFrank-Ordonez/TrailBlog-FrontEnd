81989411f6d292f158ccdf1cf5862f2ebc4dc416
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-10-29 15:21:28 +0800

    Added route for recent viewed posts

--- src/app/shared/components/right-sidebar/recent-viewed-post/recent-viewed-post.ts
+++ src/app/shared/components/right-sidebar/recent-viewed-post/recent-viewed-post.ts
@@ -1,9 +1,12 @@
 import { DatePipe } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
+import { AuthService } from '@core/services/auth.service';
 import { MessageService } from '@core/services/message.service';
 import { PostService } from '@core/services/post.service';
+import { UserService } from '@core/services/user.service';
 import { ZardDividerComponent } from '@shared/components/divider/divider.component';
 import { InitialsPipe } from '@shared/pipes/initials-pipe';
 import { TimeagoPipe } from '@shared/pipes/timeago-pipe';
@@ -15,19 +18,35 @@ import { handleHttpError } from '@shared/utils/utils';
   templateUrl: './recent-viewed-post.html',
   styleUrl: './recent-viewed-post.css',
 })
-export class RecentViewedPost implements OnInit {
+export class RecentViewedPost {
+  private readonly router = inject(Router);
   private readonly postService = inject(PostService);
   private readonly messageService = inject(MessageService);
+  private readonly userService = inject(UserService);
+  private readonly authService = inject(AuthService);
   private destroyRef = inject(DestroyRef);
 
   posts = this.postService.posts;
   recentViewedPosts = this.postService.recentViewedPosts;
   isRecentLoading = this.postService.isRecentPostsLoading;
+  isAuthenticated = this.authService.isAuthenticated;
 
   skeletonArray = Array(10).fill(0);
 
-  ngOnInit(): void {
-    this.loadRecentViewedPosts();
+  constructor() {
+    effect(() => {
+      const isAuth = this.isAuthenticated();
+
+      if (isAuth) {
+        this.loadRecentViewedPosts();
+      }
+    });
+  }
+
+  toggleRecentViewedPost(slug: string) {
+    this.router.navigate(['/post', slug]);
+
+    this.userService.setActiveUserTab('post-detail');
   }
 
   private loadRecentViewedPosts() {
