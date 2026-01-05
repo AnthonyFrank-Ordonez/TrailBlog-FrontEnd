8a6ac717cb038c31bd964259c136768471593ea3
AnthonyFrank-Ordonez <anthonyfrankordonez@yahoo.com>
2026-01-05 16:01:03 +0800

    feature(UI): Implement form group ui for the profile overview

--- src/app/shared/components/profile-overview/profile-overview.ts
+++ src/app/shared/components/profile-overview/profile-overview.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '@core/services/user.service';
+import { InitialsPipe } from '../../pipes/initials-pipe';
 
 @Component({
   selector: 'app-profile-overview',
-  imports: [],
+  imports: [ReactiveFormsModule, InitialsPipe],
   templateUrl: './profile-overview.html',
   styleUrl: './profile-overview.css',
 })
 export class ProfileOverview {
+  private userService = inject(UserService);
+  private fb = inject(FormBuilder);
 
+  currentUser = this.userService.user;
+  editMode = signal<boolean>(false);
+  selectedFileName = signal<string | null>(null);
+  profileForm = this.createForm();
+
+  constructor() {
+    this.profileForm.disable();
+  }
+
+  toggleEditMode() {
+    const newMode = !this.editMode();
+    this.editMode.set(newMode);
+
+    if (newMode) {
+      this.profileForm.enable();
+    } else {
+      this.profileForm.disable();
+    }
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      this.selectedFileName.set(file.name);
+
+      // set avatar control value
+      this.profileForm.get('avatar')?.setValue(file);
+    }
+  }
+
+  private createForm() {
+    return this.fb.group({
+      username: [this.currentUser()?.username, Validators.required],
+      email: [this.currentUser()?.email, [Validators.required, Validators.email]],
+      bio: ['', []],
+      avatar: new FormControl<File | null>(null),
+    });
+  }
 }
