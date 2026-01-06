1043702763b8fbd9ede5ba503b6c875e4fe65174
AnthonyFrank-Ordonez <anthonyfrankordonez@yahoo.com>
2026-01-06 15:02:22 +0800

    feature(UI): Implement dispplaying of error on edit profile form and added validation

--- src/app/shared/components/profile-overview/profile-overview.ts
+++ src/app/shared/components/profile-overview/profile-overview.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '@core/services/user.service';
 import { InitialsPipe } from '../../pipes/initials-pipe';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-profile-overview',
@@ -17,6 +18,16 @@ export class ProfileOverview {
   editMode = signal<boolean>(false);
   selectedFileName = signal<string | null>(null);
   profileForm = this.createForm();
+  profileFormValues = toSignal(this.profileForm.valueChanges, {
+    initialValue: this.profileForm.value,
+  });
+
+  hasChanges = computed(() => {
+    const current = this.profileFormValues();
+    const original = this.currentUser();
+
+    return current.username !== original?.username || current.email !== original?.email;
+  });
 
   constructor() {
     this.profileForm.disable();
@@ -29,6 +40,13 @@ export class ProfileOverview {
     if (newMode) {
       this.profileForm.enable();
     } else {
+      this.profileForm.reset({
+        username: this.currentUser()?.username,
+        email: this.currentUser()?.email,
+        bio: '',
+        avatar: null,
+      });
+      this.selectedFileName.set(null);
       this.profileForm.disable();
     }
   }
@@ -45,9 +63,34 @@ export class ProfileOverview {
     }
   }
 
+  onProfileSubmit() {
+    if (this.profileForm.invalid) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('username', this.profileForm.value.username!.toLowerCase().trim());
+    formData.append('email', this.profileForm.value.email!.toLowerCase().trim());
+    formData.append('bio', this.profileForm.value?.bio ?? '');
+
+    if (this.profileForm.value.avatar) {
+      formData.append('avatar', this.profileForm.value.avatar);
+    }
+  }
+
+  get usernameErrors() {
+    const control = this.profileForm.get('username');
+    return control?.errors && control.touched;
+  }
+
+  get emailErrors() {
+    const control = this.profileForm.get('email');
+    return control?.errors && control.touched;
+  }
+
   private createForm() {
     return this.fb.group({
-      username: [this.currentUser()?.username, Validators.required],
+      username: [this.currentUser()?.username, [Validators.minLength(5), Validators.required]],
       email: [this.currentUser()?.email, [Validators.required, Validators.email]],
       bio: ['', []],
       avatar: new FormControl<File | null>(null),
