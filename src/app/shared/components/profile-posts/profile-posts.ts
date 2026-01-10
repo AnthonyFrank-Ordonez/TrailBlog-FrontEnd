589c6b877b2510b3823c7627f884537fc15d4cb1
AnthonyFrank-Ordonez <anthonyfrankordonez@yahoo.com>
2026-01-10 21:04:16 +0800

    feature: display all the published posts on user in profile-posts in profile route

--- src/app/shared/components/profile-posts/profile-posts.ts
+++ src/app/shared/components/profile-posts/profile-posts.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PostService } from '@core/services/post.service';
+import { PostList } from '../post-list/post-list';
 
 @Component({
   selector: 'app-profile-posts',
-  imports: [],
+  imports: [PostList],
   templateUrl: './profile-posts.html',
   styleUrl: './profile-posts.css',
 })
 export class ProfilePosts {
+  private postService = inject(PostService);
 
+  profilePosts = this.postService.posts;
 }
