bdb7b54f2bd392b611b586008834d035b7aa0746
AnthonyFrank-Ordonez <anthonyfrankordonez@yahoo.com>
2026-01-19 15:54:06 +0800

    feature(UI): Create comment list and comment card for displaying of comments

---
+++ src/app/shared/components/comment-list/comment-card/comment-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-card',
+  imports: [],
+  templateUrl: './comment-card.html',
+  styleUrl: './comment-card.css',
+})
+export class CommentCard {
+
+}
