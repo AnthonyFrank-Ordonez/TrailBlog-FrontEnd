447c6a94355be798224f104f5c9542fce915a57c
AnthonyFrank-Ordonez <anthonyfrankordonez@yahoo.com>
2026-01-28 20:08:42 +0800

    feature: Added admin folder in pages for admin pages

---
+++ src/app/pages/admin/user-lists/user-lists.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-lists',
+  imports: [],
+  templateUrl: './user-lists.html',
+  styleUrl: './user-lists.css',
+})
+export class UserLists {
+
+}
